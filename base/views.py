d453dfff0f1b471ece77da808025abddfa5f00ed
Bolivegano <bolivegano@proton.me>
2023-09-19 17:28:54 -0600

    up to API section of video

--- base/views.py
+++ base/views.py
@@ -76,7 +76,7 @@ def home(request):
         Q(description__icontains=q)
         )
     
-    topics = Topic.objects.all()
+    topics = Topic.objects.all()[0:5]
     room_count = rooms.count()
     room_messages = Message.objects.filter(Q(room__topic__name__icontains=q))
     
@@ -173,7 +173,7 @@ def deleteMessage(request, pk):
     if request.method == 'POST':
         message.delete()
         return redirect('home')
-    return render(request, 'base/delete.html', {'obj': message})
+    return render(request, 'base/delete.html', {'obj': message.body[0:40] + " ..."})
 
 @login_required(login_url='login')
 def updateUser(request):
@@ -187,4 +187,13 @@ def updateUser(request):
             return redirect('user-profile', pk=user.id)
     
     return render(request, 'base/update-user.html', {'form': form})
+
+def topicsPage(request):
+    q = request.GET.get('q') if request.GET.get('q') is not None else ''
+    topics = Topic.objects.filter(name__icontains=q)
+    return render(request, 'base/topics.html', {'topics': topics})
+
+def activityPage(request):
+    room_messages = Message.objects.all()
+    return render(request, 'base/activity.html', {'room_messages': room_messages})
     
